fbfe402ea077b093f1c53a5787db921e0f1a1641
Mirembe Peace Mercy <peace2de@gmail.com>
2025-07-16 00:29:29 +0300

    hotspots opacity to zero

--- js/script.js
+++ js/script.js
@@ -125,7 +125,7 @@ function createPictureHotspots() {
         const material = new THREE.MeshBasicMaterial({
             color: 0x0000ff,
             transparent: true,
-            opacity: 0.3 // change back after adjusting
+            opacity: 0 // change back after adjusting
         });
         const pictureFrame = new THREE.Mesh(geometry, material);
         pictureFrame.position.copy(data.position);
@@ -152,7 +152,7 @@ function createExhibitHotspots() {
         const material = new THREE.MeshBasicMaterial({
             color: 0xff0000,
             transparent: true,
-            opacity: 0.3 // Completely invisible
+            opacity: 0 // Completely invisible
         });
         const sphere = new THREE.Mesh(geometry, material);
         sphere.position.copy(data.position);
